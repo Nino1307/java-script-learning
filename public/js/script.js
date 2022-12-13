7ac4b9c3e4b28bede51766a449492a32cfb4436d
lukinoo <luka.bliadze.2015@mail.ru>
2022-12-13 21:59:56 +0400

    ✨NEW: adding javaScript basics

--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,31 @@
-let text = "hello javascript";
-console.log(text);
+// variable keywords -> before es6 var & es6 let, const
+
+// variable types primitive | refference
+
+// primitive type
+let my_name = "nino"; // <- string type
+let my_age = 23; // <- number
+let bool = true; // <- boolean true | false
+let undef; // <- undefined
+let nul = null; // <- null
+
+// function
+function isActive() {
+  if (this.status === true) {
+    return "user is active";
+  } else {
+    return "user is NOT active";
+  }
+}
+
+// refference type
+let obj = {
+  myname: "luka",
+  myage: 21,
+  status: true,
+  isActive: isActive,
+};
+
+let res = obj.isActive();
+
+console.log(res);
